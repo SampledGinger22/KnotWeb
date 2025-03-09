21383cf3979b7182ea996276b9e593b2d1e5adbc
SampledGinger22 <mason.britsch@gmail.com>
2025-03-09 16:37:44 -0500

    Added initial under construction page pre-proper links imbedded

--- src/components/Construction.jsx
+++ src/components/Construction.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import '../style/Construction.css';
 import CastleImage from '../assets/Castle.jpg';
+import Logo from '../assets/Logo.png';
 
 const Construction = () => {
     return (
         <div className='container'>
-            <img src={CastleImage} alt="Castle" />
+            <img src={CastleImage} alt="Castle" className='backgroundImg' />
+            <div className='overlay'>
+                <div className='logo-container'>
+                    <img src={Logo} className='logo'/>
+                    <h1>Website Under Construction</h1>
+                </div>
+                <p className='summary'>In the meantime, please check out our <a href="https://www.youtube.com">YouTube</a> or <a href="https://www.example.com/signup">Sign Up for our Mail List</a></p>
+            </div>
         </div>
     );
 };
