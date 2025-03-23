51e2c37152be1c8b4fc1bf1ba6ac17a0476f52d0
SampledGinger22 <mason.britsch@gmail.com>
2025-03-23 12:23:38 -0500

    Finished v1 of header redo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,20 +7,24 @@ import Give from './Give';
 const Header = ({ isGiveVisible, toggleGivePopup }) => {
   return (
     <div>
-      <img src={CastleImage} alt="Castle" className='backgroundImg' />
-      <div className='container'>
-        <div className='lowerHeader'>
-          <img src={Logo} className='logo' />
-          <div className='lowerNav'>
-            <button onClick={toggleGivePopup}>Give Now</button>
-            <a href='#'>Serve With Us</a>
-            <a href='#'>Mission & Vision</a>
-            <a href='#'>Our Values</a>
-            <a href='#'>Our Faith</a>
-            <a href='#'>What We Do</a>
-            <a href='#'>Contact Us</a>
-          </div>
+      <img src={CastleImage} alt="Castle" className="backgroundImg" />
+      <div className="top-overlay">
+        <img src={Logo} className="logo" />
+        <button className="give-now-button" onClick={toggleGivePopup}>
+          Donate
+        </button>
+      </div>
+      <div className="lowerHeader"> {/* Moved lowerHeader directly under the image */}
+        <div className="lowerNav">
+          <a href="#">Serve With Us</a>
+          <a href="#">Mission & Vision</a>
+          <a href="#">Our Values</a>
+          <a href="#">Our Faith</a>
+          <a href="#">What We Do</a>
+          <a href="#">Contact Us</a>
         </div>
+      </div>
+      <div className="container">
         <Give isVisible={isGiveVisible} onClose={toggleGivePopup} />
       </div>
     </div>
